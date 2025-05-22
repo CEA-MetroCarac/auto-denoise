61c25f3352198d51ff648c1ab92f71466409d6cf
Nicola VIGANÒ <nicola.vigano@cea.fr>
2025-05-22 16:47:36 +0200

    Models / MS-D net: generalized to 1D and 3D signals
    Signed-off-by: Nicola VIGANÒ <nicola.vigano@cea.fr>

--- src/autoden/models/msd.py
+++ src/autoden/models/msd.py
@@ -6,14 +6,25 @@ from typing import overload, Literal
 import torch as pt
 import torch.nn as nn
 
+NDConv = {1: nn.Conv1d, 2: nn.Conv2d, 3: nn.Conv3d}
+NDBatchNorm = {1: nn.BatchNorm1d, 2: nn.BatchNorm2d, 3: nn.BatchNorm3d}
+NDPool = {1: nn.AvgPool1d, 2: nn.AvgPool2d, 3: nn.AvgPool3d}
+NDPadding = {1: nn.ReplicationPad1d, 2: nn.ReplicationPad2d, 3: nn.ReplicationPad3d}
+NDUpsampling = {1: "linear", 2: "bilinear", 3: "trilinear"}
+
+
+def _get_alignment_padding(shape: tuple[int, ...], n_levels: int = 1, kernel_size: int = 1) -> tuple[int, ...]:
+    align_size = kernel_size * 2**n_levels
+    return sum([((-s % align_size) // 2, (-s % align_size) - (-s % align_size) // 2) for s in reversed(shape)], ())
+
 
 class DilatedConvBlock(nn.Sequential):
     """Dilated convolution block (dilated_conv => BN => ReLU)."""
 
-    def __init__(self, in_ch: int, out_ch: int, dilation: int = 1, pad_mode: str = "replicate") -> None:
+    def __init__(self, in_ch: int, out_ch: int, dilation: int = 1, pad_mode: str = "replicate", ndim: int = 2) -> None:
         super().__init__(
-            nn.Conv2d(in_ch, out_ch, 3, padding=dilation, dilation=dilation, padding_mode=pad_mode),
-            nn.BatchNorm2d(out_ch),
+            NDConv[ndim](in_ch, out_ch, 3, padding=dilation, dilation=dilation, padding_mode=pad_mode),
+            NDBatchNorm[ndim](out_ch),
             nn.LeakyReLU(0.2, inplace=True),
         )
 
@@ -21,27 +32,28 @@ class DilatedConvBlock(nn.Sequential):
 class SamplingConvBlock(nn.Sequential):
     """Down-sampling convolution module (down-samp => conv => BN => ReLU => up-samp)."""
 
-    def __init__(self, in_ch: int, out_ch: int, samp_factor: int = 1) -> None:
+    def __init__(self, in_ch: int, out_ch: int, samp_factor: int = 1, ndim: int = 2) -> None:
         if samp_factor > 1:
-            pre = [nn.AvgPool2d(samp_factor)]
-            post = [nn.Upsample(scale_factor=samp_factor, mode="bilinear", align_corners=True)]
+            pre = [NDPool[ndim](samp_factor)]
+            post = [nn.Upsample(scale_factor=samp_factor, mode=NDUpsampling[ndim], align_corners=True)]
         else:
             pre = post = []
         super().__init__(
-            *pre, nn.Conv2d(in_ch, out_ch, 3, padding=1), nn.BatchNorm2d(out_ch), nn.LeakyReLU(0.2, inplace=True), *post
+            *pre, NDConv[ndim](in_ch, out_ch, 3, padding=1), NDBatchNorm[ndim](out_ch), nn.LeakyReLU(0.2, inplace=True), *post
         )
 
 
 class MSDSampBlock(nn.Module):
     """MS-D Block containing the sequence of dilated convolutional layers."""
 
-    def __init__(self, n_channels_in: int, n_features: int, n_layers: int, dilations: Sequence[int]) -> None:
+    def __init__(self, n_channels_in: int, n_features: int, n_layers: int, dilations: Sequence[int], ndim: int = 2) -> None:
         super().__init__()
         self.n_features = n_features
         self.n_layers = n_layers
         self.dilations = dilations
+        self.ndim = ndim
         convs = [
-            SamplingConvBlock(n_channels_in + n_features * ii, n_features, samp_factor=self._layer_sampling(ii))
+            SamplingConvBlock(n_channels_in + n_features * ii, n_features, samp_factor=self._layer_sampling(ii), ndim=ndim)
             for ii in range(n_layers)
         ]
         self.convs = nn.ModuleList(convs)
@@ -51,32 +63,29 @@ class MSDSampBlock(nn.Module):
         return 2 ** (self.dilations[ind % len(self.dilations)] - 1)
 
     def forward(self, x: pt.Tensor) -> pt.Tensor:
-        max_dilation_span = 3 * 2 ** (max(self.dilations) - 1)
-        img_size = x.shape[2:]
-        img_pad_total = [-s % max_dilation_span for s in img_size]
-        padding = [(s // 2, s - s // 2) for s in img_pad_total]
-        pad_input = nn.ReplicationPad2d((*padding[-2], *padding[-1]))
+        padding = _get_alignment_padding(x.shape[2:], n_levels=max(self.dilations) - 1, kernel_size=3)
+        pad_input = NDPadding[self.ndim](padding)
 
         latent = [pad_input(x)]
         for conv in self.convs:
             latent.append(conv(pt.cat(latent, dim=1)))
         latent = pt.cat(latent, dim=1)
 
-        crop_w = padding[-2][0], padding[-2][0] + img_size[-2]
-        crop_h = padding[-1][0], padding[-1][0] + img_size[-1]
-        return latent[..., crop_w[0] : crop_w[1], crop_h[0] : crop_h[1]]
+        crop = [slice(None)] * 2 + [slice(p, p + s) for p, s in zip(padding[::2], x.shape[2:])]
+        return latent[tuple(crop)]
 
 
 class MSDDilBlock(nn.Module):
     """MS-D Block containing the sequence of dilated convolutional layers."""
 
-    def __init__(self, n_channels_in: int, n_features: int, n_layers: int, dilations: Sequence[int]) -> None:
+    def __init__(self, n_channels_in: int, n_features: int, n_layers: int, dilations: Sequence[int], ndim: int = 2) -> None:
         super().__init__()
         self.n_features = n_features
         self.n_layers = n_layers
         self.dilations = dilations
+        self.ndim = ndim
         convs = [
-            DilatedConvBlock(n_channels_in + n_features * ii, n_features, dilation=self._layer_dilation(ii))
+            DilatedConvBlock(n_channels_in + n_features * ii, n_features, dilation=self._layer_dilation(ii), ndim=ndim)
             for ii in range(n_layers)
         ]
         self.convs = nn.ModuleList(convs)
@@ -91,13 +100,6 @@ class MSDDilBlock(nn.Module):
             latent.append(conv(pt.cat(latent, dim=1)))
         return pt.cat(latent, dim=1)
 
-        # latent = pt.cat([x, x.new_empty(x.shape[0], self.n_features * self.n_layers, *x.shape[2:])], dim=1)
-        # for ii, conv in enumerate(self.convs):
-        #     ii_s = x.shape[1] + self.n_features * ii
-        #     ii_e = ii_s + self.n_features
-        #     latent[:, ii_s:ii_e, ...] = conv(latent[:, :ii_s, ...])
-        # return latent
-
 
 class MSDnet(nn.Module):
     """Simple MS-D net implementation."""
@@ -111,6 +113,7 @@ class MSDnet(nn.Module):
         dilations: Sequence[int] = [1, 2, 3, 4],
         device: str = "cuda" if pt.cuda.is_available() else "cpu",
         use_dilations: bool = True,
+        ndim: int = 2,
     ) -> None:
         init_params = locals()
         del init_params["self"]
@@ -121,10 +124,10 @@ class MSDnet(nn.Module):
         self.device = device
 
         if use_dilations:
-            self.msd_block = MSDDilBlock(n_channels_in, n_features, n_layers, dilations)
+            self.msd_block = MSDDilBlock(n_channels_in, n_features, n_layers, dilations, ndim=ndim)
         else:
-            self.msd_block = MSDSampBlock(n_channels_in, n_features, n_layers, dilations)
-        self.outc = nn.Conv2d(n_channels_in + n_features * n_layers, n_channels_out, kernel_size=1)
+            self.msd_block = MSDSampBlock(n_channels_in, n_features, n_layers, dilations, ndim=ndim)
+        self.outc = NDConv[ndim](n_channels_in + n_features * n_layers, n_channels_out, kernel_size=1)
 
         self.to(self.device)
 
