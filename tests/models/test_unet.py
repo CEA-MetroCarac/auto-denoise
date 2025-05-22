106b964011aab0f15caffdc583c592d0a0f0ea09
Nicola VIGANÒ <nicola.vigano@cea.fr>
2025-05-22 11:17:45 +0200

    Models/UNet: generalize to 1D and 3D
    Signed-off-by: Nicola VIGANÒ <nicola.vigano@cea.fr>

--- tests/models/test_unet.py
+++ tests/models/test_unet.py
@@ -3,52 +3,59 @@ import torch as pt
 from autoden.models.unet import UNet, _compute_architecture, ConvBlock, DoubleConv, DownBlock, UpBlock
 
 
-def test_compute_architecture():
+@pytest.mark.parametrize("n_skip", [None, 8, 16, 64])
+def test_compute_architecture(n_skip):
     """Test the architecture computation function"""
-    encoder, decoder = _compute_architecture(n_levels=3, n_features=32, n_skip=None)
+    n_levels = 3
+    encoder, decoder = _compute_architecture(n_levels=n_levels, n_features=32, n_skip=n_skip, verbose=True)
     assert len(encoder) == 3
     assert len(decoder) == 3
     assert encoder[0] == (32, 64)
-    assert decoder[0] == (128, None, 64)
+    assert decoder[0] == (128, n_skip * (2 ** (n_levels - 1)) if n_skip is not None else None, 64)
 
 
-def test_conv_block():
+@pytest.mark.parametrize("ndim,shape", [(1, (1, 3, 64)), (2, (1, 3, 64, 64)), (3, (1, 3, 16, 32, 32))])
+def test_conv_block(ndim, shape):
     """Test the ConvBlock class"""
-    block = ConvBlock(in_ch=3, out_ch=64, kernel_size=3)
-    inp = pt.randn(1, 3, 64, 64)
+    block = ConvBlock(in_ch=3, out_ch=64, ndim=ndim, kernel_size=3)
+    inp = pt.randn(*shape)
     out = block(inp)
-    assert out.shape == (1, 64, 64, 64)
+    assert out.shape == (1, 64, *shape[2:])
 
 
-def test_double_conv():
+@pytest.mark.parametrize("ndim,shape", [(1, (1, 3, 64)), (2, (1, 3, 64, 64)), (3, (1, 3, 16, 32, 32))])
+def test_double_conv(ndim, shape):
     """Test the DoubleConv class"""
-    block = DoubleConv(in_ch=3, out_ch=64)
-    inp = pt.randn(1, 3, 64, 64)
+    block = DoubleConv(in_ch=3, out_ch=64, ndim=ndim)
+    inp = pt.randn(*shape)
     out = block(inp)
-    assert out.shape == (1, 64, 64, 64)
+    assert out.shape == (1, 64, *shape[2:])
 
 
-def test_down_block():
+@pytest.mark.parametrize("ndim,shape", [(1, (1, 3, 64)), (2, (1, 3, 64, 64)), (3, (1, 3, 16, 32, 32))])
+def test_down_block(ndim, shape):
     """Test the DownBlock class"""
-    block = DownBlock(in_ch=3, out_ch=64)
-    inp = pt.randn(1, 3, 64, 64)
+    block = DownBlock(in_ch=3, out_ch=64, ndim=ndim)
+    inp = pt.randn(*shape)
     out = block(inp)
-    assert out.shape == (1, 64, 32, 32)
+    expected_shape = tuple(s // 2 for s in shape[2:])
+    assert out.shape == (1, 64, *expected_shape)
 
 
-def test_up_block():
+@pytest.mark.parametrize("ndim,shape", [(1, (1, 64, 32)), (2, (1, 64, 32, 32)), (3, (1, 64, 8, 16, 16))])
+def test_up_block(ndim, shape):
     """Test the UpBlock class"""
-    block = UpBlock(in_ch=64, skip_ch=None, out_ch=32)
-    inp_lo_res = pt.randn(1, 64, 32, 32)
-    inp_hi_res = pt.randn(1, 64, 64, 64)
+    block = UpBlock(in_ch=shape[1], skip_ch=None, out_ch=32, ndim=ndim)
+    inp_lo_res = pt.randn(*shape)
+    inp_hi_res = pt.randn(*shape[:2], *(s * 2 for s in shape[2:]))
     out = block(inp_lo_res, inp_hi_res)
-    assert out.shape == (1, 32, 64, 64)
+    assert out.shape == (shape[0], 32, *(s * 2 for s in shape[2:]))
 
-    block = UpBlock(in_ch=64, skip_ch=8, out_ch=32)
-    inp_lo_res = pt.randn(1, 64, 32, 32)
-    inp_hi_res = pt.randn(1, 64, 64, 64)
+    block = UpBlock(in_ch=shape[1], skip_ch=8, out_ch=32, ndim=ndim)
+    inp_lo_res = pt.randn(*shape)
+    inp_hi_res = pt.randn(*shape[:2], *(s * 2 for s in shape[2:]))
     out = block(inp_lo_res, inp_hi_res)
-    assert out.shape == (1, 32, 64, 64)
+    assert out.shape == (shape[0], 32, *(s * 2 for s in shape[2:]))
 
 
 def test_unet_initialization():
@@ -60,20 +67,22 @@ def test_unet_initialization():
     assert len(model.decoder_layers) == 3
 
 
-def test_unet_forward_pass():
+@pytest.mark.parametrize("ndim,shape", [(1, (1, 3, 64)), (2, (1, 3, 64, 64)), (3, (1, 3, 16, 32, 32))])
+def test_unet_forward_pass(ndim, shape):
     """Test the UNet forward pass"""
-    model = UNet(n_channels_in=3, n_channels_out=1, n_features=32, n_levels=3)
-    inp = pt.randn(1, 3, 64, 64, device=model.device)
+    model = UNet(n_channels_in=3, n_channels_out=1, ndim=ndim, n_features=32, n_levels=3)
+    inp = pt.randn(*shape, device=model.device)
     out = model(inp)
-    assert out.shape == (1, 1, 64, 64)
+    assert out.shape == (1, 1, *shape[2:])
 
 
-def test_unet_forward_pass_with_latent():
+@pytest.mark.parametrize("ndim,shape", [(1, (1, 3, 64)), (2, (1, 3, 64, 64)), (3, (1, 3, 16, 32, 32))])
+def test_unet_forward_pass_with_latent(ndim, shape):
     """Test the UNet forward pass with latent return"""
-    model = UNet(n_channels_in=3, n_channels_out=1, n_features=32, n_levels=3)
-    inp = pt.randn(1, 3, 64, 64, device=model.device)
+    model = UNet(n_channels_in=3, n_channels_out=1, ndim=ndim, n_features=32, n_levels=3)
+    inp = pt.randn(*shape, device=model.device)
     out, latent = model(inp, return_latent=True)
-    assert out.shape == (1, 1, 64, 64)
+    assert out.shape == (1, 1, *shape[2:])
     assert isinstance(latent, pt.Tensor)
 
 
