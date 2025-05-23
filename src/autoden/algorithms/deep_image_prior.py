6be1beee5d9fbc77aa88b8b688eee05ecfb280d7
Nicola Vigano <nicola.vigano@cea.fr>
2025-05-23 23:52:37 +0200

    Algorithms: added support for 1D and 3D signals
    Signed-off-by: Nicola Vigano <nicola.vigano@cea.fr>

--- src/autoden/algorithms/deep_image_prior.py
+++ src/autoden/algorithms/deep_image_prior.py
@@ -43,8 +43,10 @@ class DIP(Denoiser):
         """
         if tgt.ndim < self.n_dims:
             raise ValueError(f"Target data should at least be of {self.n_dims} dimensions, but its shape is {tgt.shape}")
+        if tgt.ndim > self.n_dims:
+            tgt = tgt.mean(axis=tuple(np.arange(-tgt.ndim, -self.n_dims)))
 
-        inp = np.random.normal(size=tgt.shape[-self.ndims :], scale=0.25).astype(tgt.dtype)
+        inp = np.random.normal(size=tgt.shape[-self.n_dims :], scale=0.25).astype(tgt.dtype)
         mask_trn = get_random_pixel_mask(tgt.shape, mask_pixel_ratio=num_tst_ratio)
         return inp, tgt, mask_trn
 
