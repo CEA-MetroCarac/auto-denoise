65f117bbaea15eb524630054a87ebf809670b357
Nicola Vigano <nicola.vigano@cea.fr>
2025-06-15 02:21:55 +0200

    Changed the way that modules are imported
    Signed-off-by: Nicola Vigano <nicola.vigano@cea.fr>

--- src/autoden/__init__.py
+++ src/autoden/__init__.py
@@ -5,13 +5,15 @@ Unsupervised and self-supervised CNN denoising methods.
 
 from __future__ import annotations
 
-from autoden import models
-from autoden import losses
+from . import models
+from . import losses
+from . import algorithms
 
-from autoden.algorithms import *
-from autoden.models.config import NetworkParamsDnCNN, NetworkParamsMSD, NetworkParamsUNet, NetworkParamsResnet
+# Legacy
+from .algorithms import *
+from .models.config import NetworkParamsDnCNN, NetworkParamsMSD, NetworkParamsUNet, NetworkParamsResnet
 
-from autoden.debug import get_version
+from .debug import get_version
 
 __author__ = """Nicola VIGANO"""
 __email__ = "nicola.vigano@cea.fr"
--- src/autoden/algorithms/__init__.py
+++ src/autoden/algorithms/__init__.py
@@ -6,6 +6,12 @@ Implementation of denoising algorithms like Noise2Noise, Noise2Void, and Deep Im
 __author__ = """Nicola Vigano"""
 __email__ = "nicola.vigano@cea.fr"
 
+from . import denoiser
+from . import supervised
+from . import noise2noise
+from . import noise2void
+from . import deep_image_prior
+
 from .supervised import Supervised
 from .noise2noise import N2N
 from .noise2void import N2V
